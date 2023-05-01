145b3ee8c92d97e335f0f2e2094fece6e9fd2b76
Diego Rubi Salas <diegoalru@gmail.com>
2023-05-01 12:41:07 -0600

    Metodo de prueba para el API.

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,3 +1,13 @@
-export async function GET(request: Request) {
-    return new Response('Uploading file...');
+export async function POST(request: Request) {
+
+    const data = request.formData();
+    console.log(data);
+
+    return new Response(JSON.stringify({
+            message: "Hello World",
+    }), {
+        headers: {
+            "content-type": "application/json"
+        },
+    });
 }
