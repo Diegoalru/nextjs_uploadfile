c31a5e23c7649ef259bafb7a1efc84ec71fef8bc
Diego Rubí Salas <diegoalru@gmail.com>
2023-05-03 14:24:34 -0600

    Logica agregada para el manejo de los archivos enviados por el usuario.

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,13 +1,55 @@
+import {NextResponse} from "next/server";
+import {writeFile} from "fs/promises";
+import path from "path";
+import fs from "fs";
+
+
+enum ContentType {
+    JSON = "application/json",
+    TEXT = "text/plain",
+}
+
 export async function POST(request: Request) {
+    try {
+        const data = await request.formData();
+        const file = await data.get("file") as File || new File([], "empty");
 
-    const data = request.formData();
-    console.log(data);
+        if (file.size === 0) {
+            return createResponse("File is empty", 400);
+        }
 
-    return new Response(JSON.stringify({
-            message: "Hello World",
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+        const filePath = path.join(process.cwd(), "public", file.name);
+
+        if (await fileExists(filePath)) {
+            return createResponse("File already exists", 409);
+        }
+
+        await writeFile(filePath, buffer);
+
+        return createResponse("File uploaded", 200);
+    } catch (e) {
+        return createResponse(`Error: Internal error while uploading file`, 500, ContentType.TEXT);
+    }
+}
+
+async function fileExists(filePath: string): Promise<boolean> {
+    try {
+        return fs.statSync(filePath).isFile();
+    } catch (e) {
+        return false;
+    }
+}
+
+function createResponse(message: string, status: number, type: ContentType = ContentType.JSON) {
+    return new NextResponse(JSON.stringify({
+        message,
     }), {
+        status: status,
         headers: {
-            "content-type": "application/json"
-        },
+            "Content-Type": type,
+        }
     });
 }
+
